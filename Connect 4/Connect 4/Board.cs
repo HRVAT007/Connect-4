abd61781ca46835a51414cbba326ed814a619815
HRVAT007 <teostanic11@gmail.com>
2022-02-05 13:36:15 +0100

    Initial commit

---
+++ Connect 4/Connect 4/Board.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_4
+{
+    public class Board
+    {
+        public int Rows = 6;
+        public int Columns = 7;
+        private Coin[,] cell;
+
+        public Board() {
+            cell = new Coin[Rows, Columns];
+            PopulateEmptyCells();
+        }
+
+        private void PopulateEmptyCells()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    cell[row, column] = Coin.Clear;
+                }
+            }
+        }
+
+        public bool CanDrop(int column)
+        {
+            if (cell[0, column] == Coin.Clear) { return true; }
+            else { return false; }
+        }
+
+        public void DrawBoard()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("■■ ");
+                for (int column = 0; column < Columns; column++)
+                {
+                    Coin type = cell[row, column];
+                    switch(type)
+                    {
+                        case Coin.Clear:
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write(". ");
+                            break;
+                        case Coin.Red:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("O ");
+                            break;
+                        case Coin.Green:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("O ");
+                            break;
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("■■");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■");
+        }
+    }
+}
---
+++ Connect 4/Connect 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connect_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+        }
+    }
+}
